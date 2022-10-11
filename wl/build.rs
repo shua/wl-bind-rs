fcd7caab16c48af292448679648ef7daf4df7128
shua <itis@isthisa.email>
2022-10-10 20:10:09 -0400

    use prettyplease
    doesn't die on long lines like rustfmt

--- wl/build.rs
+++ wl/build.rs
@@ -9,18 +9,11 @@ fn main() -> Result<(), String> {
     let protoc_rs = wl_bindgen::gen_bindings(protoc_file)
         .map_err(|e| format!("parse src/wayland.xml: {e:?}"))?;
     let out_dir = env::var_os("OUT_DIR").unwrap();
-    let dst_file = fs::File::create(Path::new(&out_dir).join("binding.rs"))
+    let mut dst_file = fs::File::create(Path::new(&out_dir).join("binding.rs"))
         .map_err(|e| format!("create binding.rs: {e:?}"))?;
-    let mut fmt_cmd = std::process::Command::new("rustfmt")
-        .stdin(std::process::Stdio::piped())
-        .stdout(std::process::Stdio::from(dst_file))
-        .spawn()
-        .unwrap();
-    write!(fmt_cmd.stdin.as_ref().unwrap(), "{}", protoc_rs).unwrap();
-    let status = fmt_cmd.wait().unwrap();
-    if !status.success() {
-        return Err(format!("rustfmt: exited with {}", status.code().unwrap()));
-    }
+
+    let protoc_rs = syn::parse_file(&format!("{}", protoc_rs)).unwrap();
+    write!(dst_file, "{}", prettyplease::unparse(&protoc_rs)).unwrap();
     println!("cargo:rerun-if-changed=\"src/wayland.xml\"");
     Ok(())
 }
